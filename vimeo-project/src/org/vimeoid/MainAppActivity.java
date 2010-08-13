d0c88ccf1244bff3ea497f0dd03d553e5cd8c099
Vitaly Gashock <gshockv@gmail.com>
2010-08-13 12:21:54 +0300

    initial commit

---
+++ vimeo-project/src/org/vimeoid/MainAppActivity.java
@@ -0,0 +1,26 @@
+package org.vimeoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button sampleButton = (Button) findViewById(R.id.stubButton);
+        sampleButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast t = Toast.makeText(getApplicationContext(), "Vimeo Client Application", 10000);
+				t.show();
+			}
+		});
+    }
+}
