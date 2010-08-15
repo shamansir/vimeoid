c04477c0c0edd3dbf450a4d425756acee1c259e5
Ulric Wilfred <shaman.sir@gmail.com>
2010-08-15 23:10:04 +0300

    tuned up project names and stuff
    --HG--
    rename : src/org/vimeoid/MainAppActivity.java => src/org/vimeoid/VimeoActivity.java

--- src/org/vimeoid/VimeoActivity.java
+++ src/org/vimeoid/VimeoActivity.java
@@ -7,7 +7,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainAppActivity extends Activity {
+public class VimeoActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
