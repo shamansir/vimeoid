d755eab23a320c0078512f005cd2948079f944b6
Ulric Wilfred <shaman.sir@gmail.com>
2010-08-19 20:59:33 +0300

    added basic dto

---
+++ src/org/vimeoid/dto/User.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.vimeoid.dto;
+
+/**
+ * <dl>
+ * <dt>Project:</dt> <dd>vimeoid</dd>
+ * <dt>Package:</dt> <dd>org.vimeoid.dto</dd>
+ * </dl>
+ *
+ * <code>User</code>
+ *
+ * <p>Vimeo User information</p>
+ *
+ * @author Ulric Wilfred <shaman.sir@gmail.com>
+ * @date Aug 19, 2010 8:54:34 PM 
+ *
+ */
+public class User {
+
+}
---
+++ src/org/vimeoid/dto/Video.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.vimeoid.dto;
+
+/**
+ * <dl>
+ * <dt>Project:</dt> <dd>vimeoid</dd>
+ * <dt>Package:</dt> <dd>org.vimeoid.dto</dd>
+ * </dl>
+ *
+ * <code>Video video information</code>
+ *
+ * <p>Description</p>
+ *
+ * @author Ulric Wilfred <shaman.sir@gmail.com>
+ * @date Aug 19, 2010 8:55:56 PM 
+ *
+ */
+public class Video {
+
+}
