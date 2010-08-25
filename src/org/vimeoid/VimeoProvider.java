0c982979a936709fccf9499dd53ea2a6740937d8
Ulric Wilfred <shaman.sir@gmail.com>
2010-08-25 20:45:09 +0300

    matched URIs types with content types, added provider and filled up Action fields

--- src/org/vimeoid/VimeoProvider.java
+++ src/org/vimeoid/VimeoProvider.java
@@ -50,18 +50,50 @@ public class VimeoProvider extends ContentProvider {
     
     static {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "user/*/info",     USER_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/videos",   VIDEOS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/likes",    VIDEOS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/appears",  VIDEOS_LIST_URI_TYPE); // videos where user appears
+        uriMatcher.addURI(AUTHORITY, "user/*/all",      VIDEOS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/subscr",   VIDEOS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/albums",   ALBUMS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/channels", CHANNELS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/groups",   GROUPS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/*/ccreated", VIDEOS_LIST_URI_TYPE); // videos that user's contacts created
+        uriMatcher.addURI(AUTHORITY, "user/*/clikes",   VIDEOS_LIST_URI_TYPE); // videos that user's contacts liked
+        
+        uriMatcher.addURI(AUTHORITY, "video/#", VIDEO_SINGLE_URI_TYPE);
+        
+        uriMatcher.addURI(AUTHORITY, "group/*/info",   GROUP_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "group/*/videos", VIDEOS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "group/*/users",  USERS_LIST_URI_TYPE);
+        
+        uriMatcher.addURI(AUTHORITY, "channel/*/info",   CHANNEL_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "channel/*/videos", VIDEOS_LIST_URI_TYPE);
+        
+        uriMatcher.addURI(AUTHORITY, "album/#/info",   ALBUM_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "album/#/videos", VIDEOS_LIST_URI_TYPE);
+        
+        uriMatcher.addURI(AUTHORITY, "activity/*/did",       ACTIONS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "activity/*/happened",  ACTIONS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "activity/*/cdid",      ACTIONS_LIST_URI_TYPE); // contacts did
+        uriMatcher.addURI(AUTHORITY, "activity/*/chappened", ACTIONS_LIST_URI_TYPE); // happened to contacts
+        uriMatcher.addURI(AUTHORITY, "activity/*/cdid",      ACTIONS_LIST_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "activity/*/edid",      ACTIONS_LIST_URI_TYPE); // everyone did
+        
+        /*
         uriMatcher.addURI(AUTHORITY, "actions",    ACTIONS_LIST_URI_TYPE);
-        uriMatcher.addURI(AUTHORITY, "actions/#",  ACTION_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "action/#",   ACTION_SINGLE_URI_TYPE);
         uriMatcher.addURI(AUTHORITY, "albums",     ALBUMS_LIST_URI_TYPE);
-        uriMatcher.addURI(AUTHORITY, "albums/#",   ALBUM_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "album/#",    ALBUM_SINGLE_URI_TYPE);
         uriMatcher.addURI(AUTHORITY, "channels",   CHANNELS_LIST_URI_TYPE);
-        uriMatcher.addURI(AUTHORITY, "channels/#", CHANNEL_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "channel/#",  CHANNEL_SINGLE_URI_TYPE);
         uriMatcher.addURI(AUTHORITY, "groups",     GROUPS_LIST_URI_TYPE);
-        uriMatcher.addURI(AUTHORITY, "groups/#",   GROUP_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "group/#",    GROUP_SINGLE_URI_TYPE);
         uriMatcher.addURI(AUTHORITY, "users",      USERS_LIST_URI_TYPE);
-        uriMatcher.addURI(AUTHORITY, "users/#",    USER_SINGLE_URI_TYPE);
+        uriMatcher.addURI(AUTHORITY, "user/#",     USER_SINGLE_URI_TYPE);
         uriMatcher.addURI(AUTHORITY, "videos",     VIDEOS_LIST_URI_TYPE);
-        uriMatcher.addURI(AUTHORITY, "videos/#",   VIDEO_SINGLE_URI_TYPE);        
+        uriMatcher.addURI(AUTHORITY, "video/#",    VIDEO_SINGLE_URI_TYPE); */        
     }
 
     /* (non-Javadoc)
--- src/org/vimeoid/dto/Action.java
+++ src/org/vimeoid/dto/Action.java
@@ -15,7 +15,7 @@ import android.net.Uri;
  *
  * <code>Action</code>
  *
- * <p>Vimeo activity representation (currently not implemented)</p>
+ * <p>Vimeo activity representation (named as 'Action' not to correlate with Android Activities)</p>
  *
  * @author Ulric Wilfred <shaman.sir@gmail.com>
  * @date Aug 19, 2010 9:21:38 PM 
@@ -28,5 +28,40 @@ public class Action {
     
     public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.vimeo.action";
     public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.vimeo.action";
+    
+    public int type;
+    public /*long*/ String date;
+    public String[] tags;
+    
+    public int userId;
+    public String userDisplayName;
+    public String userProfileUrl;
+    
+    public String userSmallPortraitUrl;
+    public String userMediumPortraitUrl;
+    public String userLargePortraitUrl;
+    
+    public int subjectId;
+    public String subjectDisplayName;
+    public String subjectUrl;
+    
+    public String subjectSmallPortraitUrl;
+    public String subjectMediumPortraitUrl;
+    public String subjectLargePortraitUrl;
+    
+    public int videoId;
+    public String videoTitle;
+    public String videoUrl;
+    public String videoDescription;
+    public String[] videoTags;    
+    
+    public String videoSmallThumbnailUrl;
+    public String videoMediumThumbnailUrl;
+    public String videoLargeThumbnailUrl;
+    
+    public int videoLikesCount;
+    public int videoPlaysCount;
+    public int videoCommentsCount;
+    
 
 }
