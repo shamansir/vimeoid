f9907b511c706e652312baee22d0c943a0bb23da
Ulric Wilfred <shaman.sir@gmail.com>
2010-09-11 11:36:36 +0300

    working on titlebar

--- apk/gen/org/vimeoid/R.java
+++ apk/gen/org/vimeoid/R.java
@@ -26,20 +26,21 @@ public final class R {
         public static final int titlebar_bg=0x7f040006;
     }
     public static final class dimen {
-        public static final int item_info_font_size=0x7f05000c;
-        public static final int item_param_font_size=0x7f05000b;
-        public static final int item_subtitle_font_size=0x7f05000a;
-        public static final int item_text_padding=0x7f05000d;
-        public static final int item_thumb_height=0x7f050002;
-        public static final int item_thumb_width=0x7f050001;
-        public static final int item_title_font_size=0x7f050009;
-        public static final int load_more_height=0x7f050008;
-        public static final int single_thumb_height=0x7f050004;
-        public static final int single_thumb_width=0x7f050003;
-        public static final int small_progress_side=0x7f050006;
-        public static final int small_progressbar_height=0x7f050007;
+        public static final int item_info_font_size=0x7f05000d;
+        public static final int item_param_font_size=0x7f05000c;
+        public static final int item_subtitle_font_size=0x7f05000b;
+        public static final int item_text_padding=0x7f05000e;
+        public static final int item_thumb_height=0x7f050003;
+        public static final int item_thumb_width=0x7f050002;
+        public static final int item_title_font_size=0x7f05000a;
+        public static final int load_more_height=0x7f050009;
+        public static final int single_thumb_height=0x7f050005;
+        public static final int single_thumb_width=0x7f050004;
+        public static final int small_progress_side=0x7f050007;
+        public static final int small_progressbar_height=0x7f050008;
         public static final int titlebar_height=0x7f050000;
-        public static final int video_player_height=0x7f050005;
+        public static final int titlebar_icons_side=0x7f050001;
+        public static final int video_player_height=0x7f050006;
     }
     public static final class drawable {
         public static final int ajax_loader_bar_progress=0x7f020000;
