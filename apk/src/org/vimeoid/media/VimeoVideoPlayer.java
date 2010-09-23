67bf6c75bcf9ef7c6e8e4e4468d7d488382ef058
Anthony Kotenko (shaman.sir) <akotenko@iparkcorp.com>
2010-09-23 11:28:26 +0300

    back to simple playing

--- apk/src/org/vimeoid/media/VimeoVideoPlayer.java
+++ apk/src/org/vimeoid/media/VimeoVideoPlayer.java
@@ -3,8 +3,6 @@
  */
 package org.vimeoid.media;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -17,6 +15,7 @@ import org.vimeoid.util.Utils;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnErrorListener;
 import android.os.Handler;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -43,24 +42,22 @@ public final class VimeoVideoPlayer {
     public static final String TAG = "VimeoVideoPlayer";
     
     public static final String CACHE_DIR_NAME = "___v_video_player";
-    public static final String CACHE_FILES_PREFIX = "___v_video_chunk";
+    //public static final String CACHE_FILES_PREFIX = "___v_video_chunk";
     public static final String STREAM_FILE_NAME = "___v_video_streamed";
     
-    private static final int MIN_FIRST_CHUNK_SIZE = 256 << 10; // 256 kBytes
+    //private static final int MIN_FIRST_CHUNK_SIZE = 256 << 10; // 256 kBytes
     private static final int COPY_CHUNK_SIZE =  4 << 10; // 4 kBytes
-    private static final int TRANSFER_CHUNK_SIZE =  16 << 10; // 16 kBytes
+    //private static final int TRANSFER_CHUNK_SIZE =  16 << 10; // 16 kBytes
      
-    private Context context;
+    @SuppressWarnings("unused")
+	private Context context;
     private static MediaPlayer mediaPlayer;
-    private SurfaceHolder canvas;
+    //private SurfaceHolder canvas;
     
-    private final Handler handler; // UI Handler
-    private File downloadingMediaFile;    
+    @SuppressWarnings("unused")
+	private final Handler handler; // UI Handler
     private static File cacheDir;
     
-    private static int totalKbRead = 0;
-    private static int counter = 0; 
-
     private VimeoVideoPlayer() { 
         this.handler = new Handler();
     }
@@ -95,76 +92,85 @@ public final class VimeoVideoPlayer {
     }
     
     public void startPlaying(final SurfaceHolder canvas, final long videoId) {
-    	this.canvas = canvas;
-    	
     	try {
-    		
-            final InputStream videoStream = VimeoVideoStreamer.getVideoStream(videoId);
-			if (videoStream == null)
-				throw new IllegalStateException("The stream I've got is null, so I will not play anything :(");
-			
-			final File streamFile = File.createTempFile(STREAM_FILE_NAME, ".dat", cacheDir);
-			streamFile.deleteOnExit();
-			
-			Log.d(TAG, "Starting to copy stream in physical file");
-
-			FileOutputStream out = new FileOutputStream(streamFile);
-			byte buf[] = new byte[COPY_CHUNK_SIZE];
-			do {
-				int numread = videoStream.read(buf);
-				if (numread <= 0)
-					break;
-				out.write(buf, 0, numread);
-			} while (true);
-			try {
-				videoStream.close();
-			} catch (IOException ex) {
-				Log.e(TAG, "error: " + ex.getMessage(), ex);
-			}
-			
-			Log.d(TAG, "Copying finished, starting player");
-			
-		    final Runnable fireStart = new Runnable() {
-
-				@Override
-				public void run() {					
-					try {
-						mediaPlayer = new MediaPlayer();
-						mediaPlayer.setOnErrorListener(
-					                new MediaPlayer.OnErrorListener() {
-					                    public boolean onError(MediaPlayer mp, int what, int extra) {
-					                        Log.e(getClass().getName(), "Error in MediaPlayer: (" + what +") with extra (" +extra +")" );
-					                        return false;
-					                    }
-					                });
-						
-						final FileInputStream fis = new FileInputStream(streamFile);
-					    mediaPlayer.setDataSource(fis.getFD());						
-						mediaPlayer.prepare();
-						
-						Log.d(TAG, "Player prepared, musta run shortly");
-												
-					} catch (IllegalStateException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				     mediaPlayer.setDisplay(canvas);
-				     mediaPlayer.start();
-				     Log.d(TAG, "We've started!");
-				}
-				
-			};
-			handler.post(fireStart);			
-			            
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (VideoLinkRequestException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    		 Log.d(TAG, "Let's get the stream");
+    		 
+    		 final InputStream videoStream = VimeoVideoStreamer.getVideoStream(videoId);
+    		 if (videoStream == null) {
+    			 Log.e(TAG, "The returned video stream is null, so I will not play anything :(");
+    			 return;
+    		 }
+    		 
+    		 Log.d(TAG, "Creating player");
+    		 mediaPlayer = new MediaPlayer();
+    		 if (mediaPlayer == null) throw new IllegalStateException("Failed to create media player");
+    		 
+    		 mediaPlayer.setDisplay(canvas);
+    		 mediaPlayer.setAudioStreamType(2);
+    		 
+    		 mediaPlayer.setOnErrorListener(new OnErrorListener() {
+    			 @Override
+    			 	public boolean onError(MediaPlayer mp, int what, int extra) {
+    				 	Log.e(TAG, "Media player error: " + what + "/" + extra);
+    				 	return false;
+    			 	}
+    		 });
+    		 
+    		 Runnable updater = new Runnable() {
+    			 public void run() {
+    				 try {
+    					 Log.d(TAG, "Starting the media thread");
+    					 File streamFile = File.createTempFile(STREAM_FILE_NAME, ".dat", cacheDir);
+    					 streamFile.deleteOnExit();
+    		 
+    					 FileOutputStream out = new FileOutputStream(streamFile);
+    					 byte buf[] = new byte[COPY_CHUNK_SIZE];
+    					 
+    					 Log.d(TAG, "Now we will write stream to file");
+    					 
+    					 do {
+    						 int numread = videoStream.read(buf);
+    						 if (numread <= 0) break;
+    						 out.write(buf, 0, numread);
+    					 } while (true);
+    		 
+    					 Log.d(TAG, "Stream is written to the file, setting data source");
+    					 mediaPlayer.setDataSource(new FileInputStream(streamFile).getFD());
+    		 
+    					 try {
+    						 videoStream.close();
+    						 Log.d(TAG, "Video stream closed");
+    					 } catch (IOException ex) {
+    						 Log.e(TAG, "error: " + ex.getMessage(), ex);
+    					 }
+    					 
+    					 mediaPlayer.prepare();
+    					 Log.d(TAG, "Starting player! Duration: " + Utils.adaptDuration(mediaPlayer.getDuration() / 1000));
+    					 mediaPlayer.start();
+    				 } catch (IllegalArgumentException iae) {
+    					 informException(iae);
+    				 } catch (SecurityException se) {
+    					 informException(se);
+    				 } catch (IllegalStateException ise) {
+    					 informException(ise);
+    				 } catch (IOException ioe) {
+    					 informException(ioe);
+    				 }
+    			 }
+    		 
+    		 };
+    		 
+    	     new Thread(updater).start();    		 
+    	} catch (VideoLinkRequestException vlre) {
+    		 informException(vlre); 
         }
+    	
     }
-    
+    	
+ 	private void informException(Exception exception) {
+ 		 //Dialogs.makeToast(context, exception.getLocalizedMessage());
+ 	     Log.e(TAG, exception.getLocalizedMessage());
+ 	     exception.printStackTrace();
+ 	}    	
+
 }
