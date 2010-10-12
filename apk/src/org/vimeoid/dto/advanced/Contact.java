6eebf1bb54c905e3d4656af20d42150e967fceb8
Ulric Wilfred <shaman.sir@gmail.com>
2010-10-12 23:35:41 +0300

    (contact)

---
+++ apk/src/org/vimeoid/dto/advanced/Contact.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.vimeoid.dto.advanced;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * <dl>
+ * <dt>Project:</dt> <dd>vimeoid</dd>
+ * <dt>Package:</dt> <dd>org.vimeoid.dto.advanced</dd>
+ * </dl>
+ *
+ * <code>Contact</code>
+ *
+ * <p>Description</p>
+ *
+ * @author Ulric Wilfred <shaman.sir@gmail.com>
+ * @date Oct 12, 2010 11:11:04 PM 
+ *
+ */
+public class Contact extends User {
+
+    public boolean isMutual;
+    
+    public static final class FieldsKeys {
+        public static final String CONTACTS_MULTIPLE_KEY = "contacts";
+        public static final String CONTACTS_SINGLE_KEY = "contact";
+        
+        public static final String IS_MUTUAL = "mutual";
+    }
+    
+    public static boolean contactWithIdExist(JSONObject page, long testUserId) throws JSONException {
+        final JSONArray array = page.getJSONObject(FieldsKeys.CONTACTS_MULTIPLE_KEY)
+                                    .getJSONArray(FieldsKeys.CONTACTS_SINGLE_KEY);
+        for (int i = 0; i < array.length(); i++) {
+            if (array.getJSONObject(i).getLong(User.FieldsKeys.ID) == testUserId) return true;
+        }
+        return false;
+    }
+
+    public static boolean checkIsMutualById(JSONObject page, long testUserId) throws JSONException {
+        final JSONArray array = page.getJSONObject(FieldsKeys.CONTACTS_MULTIPLE_KEY)
+                                    .getJSONArray(FieldsKeys.CONTACTS_SINGLE_KEY);
+        for (int i = 0; i < array.length(); i++) {
+            final JSONObject userObj = array.getJSONObject(i); 
+            if (userObj.getLong(User.FieldsKeys.ID) == testUserId) {
+                if (userObj.getInt(FieldsKeys.IS_MUTUAL) == 1) return true;
+            }
+        }
+        return false;
+    }
+    
+}
